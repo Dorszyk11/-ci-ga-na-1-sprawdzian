7bb9d3dea4916a4feb2f9d57e4c9624e706c42c5
Dorszyk11 <tymbeixpoi@gmail.com>
2023-12-08 10:25:33 +0100

    1

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css';
+
+const Header = () => {
+    return(
+        <div className="container header">
+            <h1>Ściąga na <span>| Historie</span></h1>
+            <p>Świat w XVI w.</p>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/db/topics.js
@@ -0,0 +1,69 @@
+export const Topics = [
+  {
+    id: 1,
+    temat: "Anglia w XVI x.",
+    opis:
+      "Ogólnie to anglicy jako pierwsi zcięli władce i potem wsm to nic nie zrobili konkretnego z tym ale przynajmniej europa srała w majty.",
+    daty: "1603 - śmierć Elżbiety I (bez potomnie bo nie miała ślubu) | 1603-1625 - Jakub I Stuart | 1625-1649 - Karol I Stuart | Jak karolowi się zbuntował Londyn to ten uciekł do szwecji a tam byli innego wyznania więc go zcieli | Awans za zasługi - jak brałeś udział w akcji która zabijała kogoś ważnego to dostawałeś awans w wrmi | Oliver Cromwell - ztworzył armie nowego wzoru w 1644 i wszystkich rozgniatał | 1685 - Jakub II | 1689 - Wilhelm III"
+  },
+  {
+    id: 2,
+    temat: "Anglia w XVI x.",
+    opis:`kalwiniści dla nich się liczyła praca dzięki czemu np w polsce stan finansowy się o wiele poprawił od momentu wejścia tego wyznania
+    dzięki chrzestu moożna odtwarzać drzewa genealogiczne
+    jezuici przez 200 lat są supciu dupcio wszyscy ich lubią i wgl ale żeby zostać jezuitą trzeba mieć studia
+    jezuici wpadli na ala szkółki (kolegia jezuickie) w których przekazywali swoje poglądy dziecią
+    jezuici rozpoczęli barok
+    we francji rozpoczęły się wony religijne pomiędzy kalwinistami a katolikami 
+    1572r. - ślób katoliczki i kalwinisty i kocien wojny :D ALEEE podczas wesela była niezła jadka i zginęło 3k typa i to była noc św bartłomieja
+    1573r. - polska ogłasza że jest państwem tolerancyjnym
+    w połowie 16 w niderlandy zaczęły przechodzić na kalwinizm
+    na północy byli kalwiniści i mordowali katolików z południa
+    `
+    },
+  {
+    id: 3,
+    temat: "Demokracja szlachcicka",
+    opis:`
+      W XVI w był ruch egzekucyjny czyli szlachta wymiśliła że magnaci mają za dużo ziemi i że mają to oddać królowi.
+      Majątki króla zostały obłorzone podatkiem(25%) i poszło to na wojsko
+      Bona została królową polski - wróciła z renesansowych włoch - i zaczęła zmiany:
+      - przywiozła i rozpopularyzowała włoszczyzne, pomidory, widelec
+      - zprowadziła artystów którzy rysowali portrety(równe proporcje)
+      Rozwój szkolenictwa
+
+      Traktat o dwóch sarmacjach opisywał ziemie która "należy" do szlachty ale w XVI w nikt w to nie uwierzył ale w XVII w już przez wojny społeczeństwo zgłupiało i uwierzyli w tą księgie która sporo namieszała
+      Mikołaj rej rozpowszechnił literature w języku narodowim
+      Arasy - rzadkie tkaniny na ściane ukradzione przez niemców ",
+      daty: "1603 - śmierć Elżbiety I (bez potomnie bo nie miała ślubu) | 1603-1625 - Jakub I Stuart | 1625-1649 - Karol I Stuart | Jak karolowi się zbuntował Londyn to ten uciekł do szwecji a tam byli innego wyznania więc go zcieli | Awans za zasługi - jak brałeś udział w akcji która zabijała kogoś ważnego to dostawałeś awans w wrmi | Oliver Cromwell - ztworzył armie nowego wzoru w 1644 i wszystkich rozgniatał | 1685 - Jakub II | 1689 - Wilhelm III
+  `,
+  daty: `1374 - Obniżenie podatku poradlnego z 12 do 2 groszy z łna, Pozbawienie możliwości króla nakładania nowych podatków bez zgody szlachty, Wykupienie polskich szlachciców z niewoli
+
+  1422 - Nietykalność majątkowa szlachty(można skonfiskować jedynie na podstawie wyroku sądu), sądzenie według prawa pisanego
+  
+  1423 - ograniczono przenoszenie chłopów ze wsi do miast, wojewoda może ustalać stałe ceny prodoktów
+  
+  1430 i 1433 - nietykalność osobista szlachty i możliwość umieszczenia szlachty we więzieniu po przez wyrok sądu 
+  
+  1454 - zakaz zwoływania przez władcę pospolitego ruszenia, wprowadzania nowych praw oraz nakładania podatków bez zgody lokalnych zjazdów szlachty
+  
+  1473 - drukarnia Kraków
+  
+  1496 - zniesienie cła na towar wytworzony przez szlachtę dla siebie lub dla jej dobra, ograniczenia praw chłopów do opuszczania wsi, zakaz kupowania ziemi przez mieszczan, kontrola cen towarów rzemieślniczych 
+  
+  1505 - wszelkie zmiany praw mogły odbyć się tylko za zgodą szlachty, zakaz wprowadzania opłat za przwóz dóbr przez teren prywatny (cło prywatne)
+  
+  1580 - powstanie zamościa (powstał w załorzeniu że ma być renesansowy)
+  `
+  },
+  {
+    id: 4,
+    temat: "Król wersal.",
+    opis: `Król to słońce czyli dzięki niemu istnieje państwo i życie
+    1682 - wersal
+    Paryż był stolicą kultury i panowała moda francuska
+    Ludzie się myli bardzo bardzo rzadko przez co wszy choroby zmiany skórne itp 
+    pudrowanie się ołowiem temu nie pomagało `,
+    daty: "1682 - wersal"
+  },
+]
